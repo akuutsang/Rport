84ceb7947a0f780b9d81cebc25adfb17cf74b6c4
akutsang <akuutsang@gmail.com>
2023-09-18 01:16:28 +0100

    import intro.css, add intro container

--- rportfolio/src/components/intro/Intro.jsx
+++ rportfolio/src/components/intro/Intro.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import './intro.css'
 
 function Intro() {
   return (
-    <div>Intro</div>
+    <div className='intro'>
+        <div className="introLeft">
+            <div className="name-container">
+                <h2 className='welcome'>Welcome to my portfolio! My name is</h2>
+                <h1 className='name'>Winnifred Kaze</h1>
+            </div>
+        </div>
+        <div className="introRight">rightt</div>
+    </div>
   )
 }
 
