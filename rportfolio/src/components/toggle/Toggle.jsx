191bb389aadad09dc00e1e2e8793c69ef7059be9
akutsang <akuutsang@gmail.com>
2023-09-23 01:16:48 +0100

    add functionality to toggle

--- rportfolio/src/components/toggle/Toggle.jsx
+++ rportfolio/src/components/toggle/Toggle.jsx
@@ -1,15 +1,20 @@
 import './toggle.css';
 import Sun from "../../images/sun.png"
 import Moon from "../../images/moon.png"
-
 import React from 'react'
+import { useContext } from 'react';
+import { ThemeContext } from '../../components/Context'
 
 export default function Toggle() {
+  const theme = useContext(ThemeContext)
+  const handleClick = ()=>{
+    theme.dispatch({type: "TOGGLE"})
+  }
   return (
     <div className='Toggle'>
         <img src={Sun} alt="" className='ToggleIcon' />
         <img src={Moon} alt="" className='ToggleIcon' />
-        <div className="Togglebtn">
+        <div className="Togglebtn" onClick={handleClick} style={{left: theme.state.darkMode ? 0 : 25}}>
         </div>
     </div>
   )
