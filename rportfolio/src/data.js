6cd6094d0d7dd6a76060f7f303d8c22692e01de1
akutsang <akuutsang@gmail.com>
2023-10-19 16:40:44 +0100

    edit image path

--- rportfolio/src/data.js
+++ rportfolio/src/data.js
@@ -1,28 +1,28 @@
 export const products = [
   {
     id: 1,
-    img: "assets/calculator.png",
+    img: "/assets/calculator.png",
     link: "https://akuutsang.github.io/Calculator/",
   },
   {
     id: 2,
-    img: "assets/adminDashboard.png",
+    img: "/assets/adminDashboard.png",
     link: "https://deft-faun-58342b.netlify.app/",
   },
   {
     id: 3,
-    img: "assets/capstoneProject.png",
+    img: "/assets/capstoneProject.png",
     link: "https://phildeoner.github.io/Capstone-Project/index.html",
   },
   {
     id: 4,
-    img: "assets/sketchPad.png",
+    img: "/assets/sketchPad.png",
     link: "https://github.com/akuutsang/Etch-A-Sketch",
   },
 
   {
     id: 5,
-    img: "assets/prs.png",
+    img: "/assets/prs.png",
     link: "rportfolio/public/assets/prs.png",
   },
 ];
