db2a01867678e2192fb0053ac345136651ca9c8e
akutsang <akuutsang@gmail.com>
2023-10-19 13:31:54 +0100

    install gh-pages

--- rportfolio/src/components/Home.jsx
+++ rportfolio/src/components/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import About from "./about/About";
+import Contact from "./contact/Contact";
+import Intro from "./intro/Intro";
+import ProductsList from "./ProductList/ProductsList";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <About />
+      <Intro />
+      <ProductsList />
+      <Contact />
+    </div>
+  );
 }
 
 export default Home;
